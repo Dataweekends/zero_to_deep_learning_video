43b023970e3b8cf90ec45be578a353cc8a7a26d7
Francesco Mosconi <git@mosconi.me>
2017-08-08 17:34:59 -0700

    fixed travis

--- tests/test_nb.py
+++ tests/test_nb.py
@@ -2,8 +2,7 @@ import subprocess
 import tempfile
 
 
-def _notebook_run(path):
-    """Execute a notebook via nbconvert"""
+def _exec_notebook(path):
     with tempfile.NamedTemporaryFile(suffix=".ipynb") as fout:
         args = ["jupyter", "nbconvert", "--to", "notebook", "--execute",
                 "--ExecutePreprocessor.timeout=1000",
@@ -12,64 +11,64 @@ def _notebook_run(path):
 
 
 def test_0():
-    _notebook_run('course/0_Check_Environment.ipynb')
+    _exec_notebook('course/0_Check_Environment.ipynb')
 
 
 def test_1():
-    _notebook_run('course/1 First Deep Learning Model.ipynb')
+    _exec_notebook('course/1 First Deep Learning Model.ipynb')
 
 
 def test_2():
-    _notebook_run('course/2 Data.ipynb')
+    _exec_notebook('course/2 Data.ipynb')
 
 
 def test_3():
-    _notebook_run('course/3 Machine Learning.ipynb')
+    _exec_notebook('course/3 Machine Learning.ipynb')
 
 
 def test_4():
-    _notebook_run('course/4 Deep Learning Intro.ipynb')
+    _exec_notebook('course/4 Deep Learning Intro.ipynb')
 
 
 def test_5():
-    _notebook_run('course/5 Gradient Descent.ipynb')
+    _exec_notebook('course/5 Gradient Descent.ipynb')
 
 
 def test_6():
-    _notebook_run('course/6 Convolutional Neural Networks.ipynb')
+    _exec_notebook('course/6 Convolutional Neural Networks.ipynb')
 
 
 def test_8():
-    _notebook_run('course/8 Recurrent Neural Networks.ipynb')
+    _exec_notebook('course/8 Recurrent Neural Networks.ipynb')
 
 
 def test_9():
-    _notebook_run('course/9 Improving performance.ipynb')
+    _exec_notebook('course/9 Improving performance.ipynb')
 
 
 def test_2_sol():
-    _notebook_run('solutions/2 Data exploration Exercises Solution.ipynb')
+    _exec_notebook('solutions/2 Data exploration Exercises Solution.ipynb')
 
 
 def test_3_sol():
-    _notebook_run('solutions/3 Machine Learning Exercises Solution.ipynb')
+    _exec_notebook('solutions/3 Machine Learning Exercises Solution.ipynb')
 
 
 def test_4_sol():
-    _notebook_run('solutions/4 Deep Learning Intro Exercises Solution.ipynb')
+    _exec_notebook('solutions/4 Deep Learning Intro Exercises Solution.ipynb')
 
 
 def test_5_sol():
-    _notebook_run('solutions/5 Gradient Descent Exercises Solution.ipynb')
+    _exec_notebook('solutions/5 Gradient Descent Exercises Solution.ipynb')
 
 
 def test_6_sol():
-    _notebook_run('solutions/6 Convolutional Neural Networks Exercises Solution.ipynb')
+    _exec_notebook('solutions/6 Convolutional Neural Networks Exercises Solution.ipynb')
 
 
 def test_8_sol():
-    _notebook_run('solutions/8 Recurrent Neural Networks Exercises Solutions.ipynb')
+    _exec_notebook('solutions/8 Recurrent Neural Networks Exercises Solutions.ipynb')
 
 
 def test_9_sol():
-    _notebook_run('solutions/9 Improving performance Exercises Solutions.ipynb')
+    _exec_notebook('solutions/9 Improving performance Exercises Solutions.ipynb')
